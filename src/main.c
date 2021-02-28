0b86b703c18c4fc51adb2225b113a7f2d47862bc
Jonathan Nilsson <jonathan@voysys.se>
2021-02-28 18:59:50 +0100

    Refactor

--- src/main.c
+++ src/main.c
@@ -21,40 +21,30 @@ int main(void)
     gpio_init(LED_PIN_R2);
     gpio_set_dir(LED_PIN_R2, GPIO_OUT);
 
+    const float conversion_factor = 3.3f / (1 << 12);
+
     adc_init();
 
-    adc_gpio_init(32);
     adc_gpio_init(31);
+    adc_gpio_init(32);
 
     for (;;)
     {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
-
         adc_select_input(0);
-        const float conversion_factor = 3.3f / (1 << 12);
         const float adc0 = conversion_factor * (float)adc_read();
 
         adc_select_input(1);
-        const float adc1 = conversion_factor * (float)adc_read();    
+        const float adc1 = conversion_factor * (float)adc_read();
 
-        if (adc0 > 0.95f)
-        {
-            gpio_put(LED_PIN_R1, 1);
-            sleep_ms(500);
-            gpio_put(LED_PIN_R1, 0);
-            sleep_ms(500);
-        }
+        gpio_put(LED_PIN, 1);
+        gpio_put(LED_PIN_R1, adc0 > 0.90f ? 1 : 0);
+        gpio_put(LED_PIN_R2, adc1 > 0.90f ? 1 : 0);
+        sleep_ms(500);
 
-        if (adc1 > 0.95f)
-        {
-            gpio_put(LED_PIN_R2, 1);
-            sleep_ms(500);
-            gpio_put(LED_PIN_R2, 0);
-            sleep_ms(500);
-        }
+        gpio_put(LED_PIN, 0);
+        gpio_put(LED_PIN_R1, 0);
+        gpio_put(LED_PIN_R2, 0);
+        sleep_ms(500);
 
         printf("adc0: %.5f, adc1: %.5f\n", adc0, adc1);
     }
